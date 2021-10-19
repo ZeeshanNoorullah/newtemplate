10462b8d0ea8f01b9f3d6d3e7434bc2ae7d6d91f
Zeeshan Bug <zeeshannoorullah4@gmail.com>
2021-10-19 23:19:29 +0500

    first Commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+  // $(window).on("scroll", function() {
+  //   if($(window).scrollTop() > 5) {
+  //       $(".centering").addClass("bg-white");
+  //   } else {
+  //       //remove the background property so it comes transparent again (defined in your css)
+  //      $(".centering").removeClass("bg-white");
+  //   }
+  // });
